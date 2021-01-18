4e24d80ef24188d37bb81479ab08a63d035b7153
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-01-19 00:12:25 +0900

    #3 Add tests for deepl module

---
+++ tests/test_deepl.py
@@ -0,0 +1,13 @@
+# third-party packages
+from astro_ph.deepl import Language, translate
+from typing_extensions import Final
+
+
+# constants
+TEXT_EN: Final[str] = "This is a test script."
+TEXT_JA: Final[str] = "これはテストスクリプトです。"
+
+
+# main features
+def test_translate():
+    assert translate(TEXT_EN, Language.JA) == TEXT_JA
