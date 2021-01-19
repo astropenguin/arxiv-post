d480417e6d18d91f89696e84bd129b2c6aaea058
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-01-19 18:55:43 +0900

    #4 Add article class

--- astro_ph/arxiv.py
+++ astro_ph/arxiv.py
@@ -3,22 +3,69 @@ __all__ = ["Query"]
 
 # standard library
 from datetime import date, datetime, time, timedelta
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
 from typing import Optional, Sequence, Union
 
 
 # third-party packages
 from typing_extensions import Final
+from .deepl import Driver, Language, translate
 
 
 # constants
 ABS: Final[str] = "abs"
 AND: Final[str] = "AND"
+ARXIV_URL: Final[str] = "arxiv_url"
+AUTHORS: Final[str] = "authors"
 CAT: Final[str] = "cat"
 DATE: Final[str] = "submittedDate"
 DATE_FORMAT: Final[str] = "%Y%m%d%H%M%S"
 OR: Final[str] = "OR"
+SUMMARY: Final[str] = "summary"
+TAGS: Final[str] = "tags"
+TERM: Final[str] = "term"
+TITLE: Final[str] = "title"
 TO: Final[str] = "TO"
+
+
+# main features
+@dataclass
+class Article:
+    """Article class to store an article information."""
+
+    title: str  #: Title of the article.
+    authors: Sequence[str]  #: Author(s) of the article.
+    summary: str  #: Summary of the article.
+    categories: Sequence[str]  #: arXiv categories.
+    arxiv_url: str  #: arXiv URL to the article.
+
+    def __post_init__(self) -> None:
+        self.title = self.title.replace("\n", " ")
+        self.summary = self.summary.replace("\n", " ")
+
+    @classmethod
+    def from_arxiv_result(cls, result: dict) -> "Article":
+        """Return an article from a result of arXiv query."""
+        return cls(
+            title=result[TITLE],
+            authors=result[AUTHORS],
+            summary=result[SUMMARY],
+            categories=[tag[TERM] for tag in result[TAGS]],
+            arxiv_url=result[ARXIV_URL],
+        )
+
+    def translate(
+        self,
+        lang_to: Language = Language.AUTO,
+        lang_from: Language = Language.AUTO,
+        driver: Driver = Driver.CHROME,
+    ) -> "Article":
+        """Return an article whose title and summary are translated."""
+        title = translate(self.title, lang_to, lang_from, driver)
+        summary = translate(self.summary, lang_to, lang_from, driver)
+        return replace(self, title=title, summary=summary)
+
+
 @dataclass
 class Query:
     """Query class to search for articles in arXiv."""
