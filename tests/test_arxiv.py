24a161b290ddc9027906a29882395ab09b879847
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-01-20 12:45:50 +0900

    #4 Add tests for arxiv module

---
+++ tests/test_arxiv.py
@@ -0,0 +1,17 @@
+# third-party packages
+from astro_ph.arxiv import search
+from typing_extensions import Final
+
+
+# constants
+DATE_START: Final[str] = "2021-01-01"
+DATE_END: Final[str] = "2021-01-02"
+KEYWORDS: Final[tuple] = "galaxy",
+CATEGORIES: Final[tuple] = "astro-ph.GA",
+N_ARTICLES: Final[int] = 4
+
+
+# test functions
+def test_search():
+    articles = search(DATE_START, DATE_END, KEYWORDS, CATEGORIES)
+    assert len(articles) == N_ARTICLES
