86dc795579fe8bc9d9bfe9f4d86468332cf56784
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-10-28 18:24:40 +0900

    #53 Add module import to package root

--- astro_ph/__init__.py
+++ astro_ph/__init__.py
@@ -1,4 +1,5 @@
 # flake8: noqa
+# type: ignore
 __author__ = "Akio Taniguchi"
 __version__ = "0.2.4"
 
@@ -6,5 +7,7 @@ __version__ = "0.2.4"
 # submodules
 from . import article
 from . import apps
+from . import constants
+from . import detex
 from . import search
 from . import translate
