c31cc8ace006232cee29e140ecf60e38ee378686
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-10-28 18:20:36 +0900

    #53 Update apps module

--- astro_ph/apps.py
+++ astro_ph/apps.py
@@ -1,84 +1,49 @@
 # standard library
-import asyncio
 from dataclasses import dataclass
-from logging import getLogger
-from typing import (
-    AsyncIterable,
-    Awaitable,
-    Callable,
-    Dict,
-    Iterable,
-    Optional,
-    TypeVar,
-)
+from typing import Callable, Dict, Iterable, List, Optional, Sequence, Union
 
 
-# third-party packages
-from aiohttp import ClientSession
-from typing_extensions import Final, Protocol
-from .translate import Translator
-from .search import Article
+# dependencies
+import requests
+from typing_extensions import Protocol
 
 
-# constants
-TIMEOUT: Final[int] = 60
-N_CONCURRENT: Final[int] = 10
-
-
-# module-level logger
-logger = getLogger(__name__)
+# submodules
+from .article import Article
 
 
 # type hints
-S = TypeVar("S")
-T = TypeVar("T")
-Payload = Dict[str, str]
+Element = Dict[str, str]
+Payload = Dict[str, Union[List[Element], str]]
 
 
 class App(Protocol):
-    """Protocol that defines application objects."""
-
-    translator: Translator  #: Translator object.
-    n_concurrent: int  #: Number of simultaneous post execution.
-    timeout: int  #: Timeout for each post execution.
-
-    async def post(self, articles: AsyncIterable[Article]) -> Awaitable:
-        """Translate and post articles to somewhere."""
+    def post(self, articles: Sequence[Article]) -> None:
         ...
 
 
-# data classes
 @dataclass
 class Slack:
-    """Class for posting articles to Slack by incoming webhook."""
+    webhook_url: str
 
-    translator: Translator  #: Translator object.
-    n_concurrent: int = N_CONCURRENT  #: Number of simultaneous post execution.
-    timeout: int = TIMEOUT  #: Timeout for each post execution (in seconds).
-    webhook_url: str = ""  #: URL of Slack incoming webhook.
+    def post(self, articles: Iterable[Article]) -> None:
+        """Post articles to Slack."""
+        for article in articles:
+            requests.post(self.webhook_url, json=self.to_payload(article))
 
-    async def post(self, articles: AsyncIterable[Article]) -> Awaitable[None]:
-        """Translate and post articles to Slack."""
-        await amap(self._post, articles, self.n_concurrent, self.timeout)
+    def to_payload(self, article: Article) -> Payload:
+        """Convert an article to a payload for Slack post."""
+        if article.original is None:
+            original = article
+        else:
+            original = article.original
 
-    async def _post(self, article: Article) -> Awaitable[None]:
-        """Translate and post an article to Slack."""
-        translated = await self.translator.translate(article)
-        payload = self._to_payload(article, translated)
-
-        async with ClientSession() as client:
-            async with client.post(self.webhook_url, json=payload) as resp:
-                logger.debug(f"{str(resp.url):.300}...")
-
-        await asyncio.sleep(3.0)
-
-    def _to_payload(self, original: Article, translated: Article) -> Payload:
-        """Convert article to payload for Slack post."""
         divider = self.divider()
-        header = self.header(self.plain_text(translated.title))
+        header = self.header(self.plain_text(article.title))
         title = self.section(self.mrkdwn(f"*Title:* {original.title}"))
         authors = self.section(self.mrkdwn(f"*Authors:* {', '.join(original.authors)}"))
-        summary = self.section(self.mrkdwn(f"*Summary:* {translated.summary}"))
+        summary = self.section(self.mrkdwn(f"*Summary:* {article.summary}"))
+
         buttons = self.actions(
             elements=[
                 self.button(
@@ -92,85 +57,18 @@ class Slack:
             ]
         )
 
-        blocks = [divider, header, title, authors, summary, buttons, divider]
-        return dict(text=translated.title, blocks=blocks)
+        return dict(
+            text=article.title,
+            blocks=[divider, header, title, authors, summary, buttons, divider],
+        )
 
-    def __getattr__(self, type: str) -> Callable[[str], Payload]:
+    def __getattr__(self, type: str) -> Callable[..., Element]:
         """Generate a function to create elements of Slack payload."""
 
-        def element(text: Optional[str] = None, **params) -> Payload:
+        def element(text: Optional[str] = None, **params: str) -> Element:
             if text is None:
                 return dict(type=type, **params)
 
             return dict(text=text, type=type, **params)
 
         return element
-
-
-# helper functions
-async def amap(
-    afunc: Callable[[S], Awaitable[T]],
-    aiterable: AsyncIterable[S],
-    n_concurrent: int = N_CONCURRENT,
-    timeout: int = TIMEOUT,
-) -> Awaitable[Iterable[T]]:
-    """Async map function.
-
-    Args:
-        afunc: Coroutine function.
-        aiterable: Async iterable that yields args of afunc.
-        n_concurrent: Number of concurrent execution.
-        timeout: Timeout for each afunc execution (in seconds).
-
-    Returns:
-        Generator that yields results of afunc.
-
-    """
-    queue_in = asyncio.Queue()
-    queue_out = asyncio.Queue()
-
-    # step 1: create consumer coroutines
-    async def consume() -> Awaitable[None]:
-        async def _consume() -> Awaitable[None]:
-            arg = await queue_in.get()
-            coro = asyncio.wait_for(afunc(arg), timeout)
-
-            try:
-                result = await coro
-            except asyncio.TimeoutError as err:
-                result = err
-            finally:
-                await queue_out.put(result)
-                queue_in.task_done()
-
-        try:
-            while True:
-                await _consume()
-        except asyncio.CancelledError:
-            pass
-
-    consumers = []
-
-    for _ in range(n_concurrent):
-        consumer = asyncio.create_task(consume())
-        consumers.append(consumer)
-
-    # step 2: create register coroutine
-    async def register() -> Awaitable[None]:
-        async for arg in aiterable:
-            await queue_in.put(arg)
-
-        await queue_in.join()
-
-        for consumer in consumers:
-            consumer.cancel()
-
-    # step 3: run coroutines concurrently
-    await asyncio.gather(register(), *consumers)
-
-    # step 4: return results as a generator
-    def results():
-        while not queue_out.empty():
-            yield queue_out.get_nowait()
-
-    return results()
