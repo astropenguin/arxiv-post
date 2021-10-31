d01e4c5390c413ca5bbbe90244bd509f9fbf65f6
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-10-31 14:08:07 +0900

    #68 Update default number of concurrent translations

--- arxiv_post/constants.py
+++ arxiv_post/constants.py
@@ -17,5 +17,5 @@ START_DATE = "3 days ago at midnight in UTC"
 END_DATE = "2 days ago at midnight in UTC"
 LANGUAGE_FROM = "en"
 LANGUAGE_TO = "auto"
-N_CONCURRENT = 10
+N_CONCURRENT = 5
 TIMEOUT = 30.0
