b2c8d6784e3158620348065afcf3084bf1ddc53e
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-07-29 07:16:21 +0000

    #100 Update tests for deepl module

--- tests/test_deepl.py
+++ tests/test_deepl.py
@@ -1,12 +1,31 @@
+# standard library
+from typing import List, Tuple, TypeVar
+
+
 # dependencies
-from arxiv_post.deepl import translate
+from arxiv_post.article import Article
+from arxiv_post.deepl import Translatable, translate
+from pytest import mark
 
 
-# constants
-TEXT_EN = "This is a test script."
-TEXT_JA = "これはテストスクリプトです。"
+# type hints
+TL = TypeVar("TL", bound=Translatable)
+
+
+# test data
+testdata: List[Tuple[Translatable, Translatable]] = [
+    (
+        "This is a test script.",
+        "これはテストスクリプトです。",
+    ),
+    (
+        Article("Title of the paper", [], "Summary of the paper", ""),
+        Article("論文タイトル", [], "論文の概要", ""),
+    ),
+]
 
 
 # test functions
-def test_translate() -> None:
-    assert translate([TEXT_EN], "ja") == [TEXT_JA]
+@mark.parametrize("original, translated", testdata)
+def test_translate(original: TL, translated: TL) -> None:
+    assert translate([original], "ja") == [translated]
