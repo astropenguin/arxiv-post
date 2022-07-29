f5cfe1e299b5ef5389e84d9446d01dbd4d51ebb1
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-07-29 04:41:25 +0000

    #100 Add error handling in replacing translatables

--- arxiv_post/deepl.py
+++ arxiv_post/deepl.py
@@ -176,7 +176,10 @@ async def _translate(translatable: TL, page: Page, timeout: float) -> TL:
         if not (content := content.strip()):
             continue
 
-        return translatable.replace(original, content)
+        try:
+            return translatable.replace(original, content)
+        except ValueError:
+            break
 
     logger.warn(f"Failed to translate: {shorten(original, 50)!r}")
     return translatable
